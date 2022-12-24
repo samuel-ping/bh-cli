bdd38a135b382826e98b14b49dbaa6250acf3f17
Sam Ping <samuelping42@gmail.com>
2022-12-24 02:09:07 -0500

    Add option to replace transparent background with solid color.

--- commands/encode.js
+++ commands/encode.js
@@ -2,7 +2,11 @@ const { encode: blurhashEncode } = require('blurhash');
 const sharp = require('sharp');
 
 async function encode(imageFile, options) {
-    const { err, data, info } = await sharp(imageFile).raw().toBuffer({ resolveWithObject: true })
+    const { err, data, info } = await sharp(imageFile)
+        .raw()
+        .flatten({ background: options.replaceAlpha }) // replace alpha channel with specified color
+        .ensureAlpha() // seems redundant, but need to add the alpha channel back
+        .toBuffer({ resolveWithObject: true });
     
     const pixelArray = new Uint8ClampedArray(data.buffer);
 
--- index.js
+++ index.js
@@ -13,17 +13,18 @@ program
 program.command('encode')
     .description('Encodes the image file into a BlurHash string and displays it in the terminal.')
     .argument('<image_file>', 'The image file to convert into a BlurHash string.')
-    .option('-x, --componentX <int>', 'Number of horizontal components', '4')
-    .option('-y, --componentY <int>', 'Number of vertical components', '4')
+    .option('-a, --replace-alpha <hex_color>', 'Replaces the alpha channel from the image file with specified color before encoding.', '#ffffff')
+    .option('-x, --componentX <number>', 'Number of horizontal components', 4)
+    .option('-y, --componentY <number>', 'Number of vertical components', 4)
     .action(encode);
 
 program.command('decode')
     .description("Decodes the BlurHash string into an image file and displays it in the terminal.")
     .argument('<blurhash_string>', 'The BlurHash string to convert into an image file.')
-    .requiredOption('-f, --filename <string>', 'Name of the output image file.')
+    .requiredOption('-f, --filename <name>', 'Name of the output image file.')
     .option('-t, --type <string>', 'Type of the output image file.', 'png')
-    .option('-w, --width <int>', 'Width of the output image file.', 32)
-    .option('-h, --height <int>', 'Height of the output image file.', 32)
+    .option('-w, --width <width>', 'Width of the output image file.', 32)
+    .option('-h, --height <height>', 'Height of the output image file.', 32)
     .action(decode);
 
 program.command('validate')
