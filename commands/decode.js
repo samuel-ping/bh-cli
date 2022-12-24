ed2b9bf0428fa9b9a6cfcfc2f2abb43ac74a1c02
Sam Ping <samuelping42@gmail.com>
2022-12-24 00:37:38 -0500

    Add decode command

---
+++ commands/decode.js
@@ -0,0 +1,16 @@
+const { decode: blurhashDecode } = require('blurhash')
+const sharp = require('sharp');
+
+async function decode(blurhash, options) {
+    const pixelArray = blurhashDecode(blurhash, options.width, options.height);
+
+    const {err, info} = await sharp(pixelArray, {
+        raw: {
+            width: options.width,
+            height: options.height,
+            channels: 4
+        }
+    }).toFile(`${options.filename}.${options.type}`);
+}
+
+module.exports = decode
--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const { program } = require('commander')
 
 const encode = require('./commands/encode')
+const decode = require('./commands/decode')
 
 program
   .name('blurhash-cli')
@@ -14,5 +15,14 @@ program.command('encode')
     .option('-x, --componentX <int>', 'Number of horizontal components', '4')
     .option('-y, --componentY <int>', 'Number of vertical components', '4')
     .action(encode)
+
+program.command('decode')
+    .description("Decodes the BlurHash string into an image file and displays it in the terminal.")
+    .argument('<blurhash_string>', 'The BlurHash string to convert into an image file.')
+    .requiredOption('-f, --filename <string>', 'Name of the output image file.')
+    .option('-t, --type <string>', 'Type of the output image file.', 'png')
+    .option('-w, --width <int>', 'Width of the output image file.', 32)
+    .option('-h, --height <int>', 'Height of the output image file.', 32)
+    .action(decode)
 
 program.parse()
